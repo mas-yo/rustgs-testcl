01e6e75b76b29d24f4d808c75975f1d940667717
mas-yo <mypianoplayer@gmail.com>
2019-04-16 18:59:28 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use rand;
 use rand::Rng;
 use std::sync::{Arc,RwLock};
 use std::env;
+use uuid::Uuid;
 
 mod command;
 
@@ -16,8 +17,8 @@ fn start_client(addr: &str) {
     let addr = format!("{}:18290", addr).parse().unwrap();
 
     let mut rng = rand::thread_rng();
-    let name:u32 = rng.gen();
-    let name = format!("{}", name);
+    let name:String = Uuid::new_v4().to_hyphenated().to_string();
+//    let name = format!("{}", name);
     let client = TcpStream::connect(&addr).and_then(move|stream| {
         let framed = Framed::new(stream, Codec::default());
         let (tx,rx) = framed.split();
@@ -34,16 +35,21 @@ fn start_client(addr: &str) {
                     opt_tx = Some(tx.send(C2S::ResponseLoginInfo(name.clone()))).wait().unwrap();
                 },
                 S2C::ShowUI(uiid,show) if *uiid==2 => {
-                    for i in 0..100 {
+                    for i in 0..1000 { //ここが大きすぎると返事がこない
+                        let mut text = (0..100).map(|_|"X").collect::<String>();
+                        // text.insert_str(0, &text.clone());
+                        // text.insert_str(0, &text.clone());
+                        // text.insert_str(0, &text.clone());
+                        // text.insert_str(0, &text.clone());
                         let tx = opt_tx.take().unwrap();
-                        opt_tx = Some(tx.send(C2S::InputText(format!("hello {}", i))).wait().unwrap());
+                        opt_tx = Some(tx.send(C2S::InputText(text)).wait().unwrap());
                     }
                     let tx = opt_tx.take().unwrap();
                     opt_tx = Some(tx.send(C2S::InputText("bye".to_string())).wait().unwrap());
                     // return Ok(false);
                 },
                 S2C::AddText(uiid,text) => {
-                    println!("recv: {}", text);
+                    println!("recv characters: {}", text.len());
                     if str::ends_with(text, "bye") {
                         return Ok(false);
                     }
@@ -68,7 +74,7 @@ pub fn main() -> Result<(), Box<std::error::Error>> {
     let args:Vec<String> = env::args().collect();
     let task = Ok(()).into_future().and_then(move|_|{
         let addr = &args[1];
-        for _ in 0..3 {
+        for _ in 0..1 {
             start_client(addr);
         }
         Ok(())
