cfe8c49771177b929ccf1c141d2f9b5577613f5c
mas-yo <mypianoplayer@gmail.com>
2019-04-17 17:59:56 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,14 @@ fn start_client(addr: &str) {
 
     let mut rng = rand::thread_rng();
     let name:String = Uuid::new_v4().to_hyphenated().to_string();
+    let bye_text = format!("bye {}", name);
 //    let name = format!("{}", name);
     let client = TcpStream::connect(&addr).and_then(move|stream| {
         let framed = Framed::new(stream, Codec::default());
         let (tx,rx) = framed.split();
         let mut opt_tx = Some(tx);
         let receive = rx.take_while(move|cmd|{
-            println!("{:?}", cmd);
+            // println!("{:?}", cmd);
             match cmd {
                 S2C::ShowUI(uiid,show) if *uiid==1001 => {
                     let tx = opt_tx.take().unwrap();
@@ -35,8 +36,8 @@ fn start_client(addr: &str) {
                     opt_tx = Some(tx.send(C2S::ResponseLoginInfo(name.clone()))).wait().unwrap();
                 },
                 S2C::ShowUI(uiid,show) if *uiid==2 => {
-                    for i in 0..1000 { //ここが大きすぎると返事がこない
-                        let mut text = (0..100).map(|_|"X").collect::<String>();
+                    for i in 0..100 { //ここが大きすぎると返事がこない
+                        let mut text = (0..1000).map(|_|"X").collect::<String>();
                         // text.insert_str(0, &text.clone());
                         // text.insert_str(0, &text.clone());
                         // text.insert_str(0, &text.clone());
@@ -45,12 +46,13 @@ fn start_client(addr: &str) {
                         opt_tx = Some(tx.send(C2S::InputText(text)).wait().unwrap());
                     }
                     let tx = opt_tx.take().unwrap();
-                    opt_tx = Some(tx.send(C2S::InputText("bye".to_string())).wait().unwrap());
+                    opt_tx = Some(tx.send(C2S::InputText(bye_text.clone()))).wait().unwrap();
                     // return Ok(false);
                 },
                 S2C::AddText(uiid,text) => {
-                    println!("recv characters: {}", text.len());
-                    if str::ends_with(text, "bye") {
+                    println!("{:?} recv characters: {}", std::time::SystemTime::now(), text.len(), );
+                    if text.ends_with(&bye_text) {
+                        println!("disconnect.");
                         return Ok(false);
                     }
                 }
@@ -66,6 +68,7 @@ fn start_client(addr: &str) {
         println!("connection error = {:?}", err);
     });
 
+    println!("start client");
     tokio::spawn(client);
 }
 
@@ -74,7 +77,7 @@ pub fn main() -> Result<(), Box<std::error::Error>> {
     let args:Vec<String> = env::args().collect();
     let task = Ok(()).into_future().and_then(move|_|{
         let addr = &args[1];
-        for _ in 0..1 {
+        for _ in 0..2 {
             start_client(addr);
         }
         Ok(())
