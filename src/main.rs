ea887193e39e5cbed8199b0a659f4520c076bb31
mas-yo <mypianoplayer@gmail.com>
2019-05-29 16:41:50 +0900

    show avg time

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,25 @@ use std::net::SocketAddr;
 use tokio::codec::{Decoder, Encoder, Framed};
 use rand;
 use rand::Rng;
-use std::sync::{Arc,RwLock};
+use std::sync::{Arc,RwLock,Mutex};
 use std::env;
 use uuid::Uuid;
+use lazy_static::lazy_static;
 
 mod command;
 
 use crate::command::*;
+
+lazy_static! {
+    static ref CONNS_COUNT : Arc<Mutex<u32>> = {
+        Arc::new(Mutex::new(0u32))
+    };
+}
+lazy_static! {
+    static ref TIME_SUM : Arc<Mutex<u128>> = {
+        Arc::new(Mutex::new(0u128))
+    };
+}
 
 fn start_client(addr: String, id:u32, name: String, tx_next: futures::sync::mpsc::Sender<(u32,String)>) {
     let addr = format!("{}:18290", addr).parse().unwrap();
@@ -26,7 +38,10 @@ fn start_client(addr: String, id:u32, name: String, tx_next: futures::sync::mpsc
         let framed = Framed::new(stream, Codec::default());
         let (tx,rx) = framed.split();
         let mut tx = tx.wait();
+        let mut send_at = None;
+
         let receive = rx.take_while(move|cmd|{
+
             // println!("recv {:?}", cmd);
             match cmd {
                 S2C::ShowUI(uiid,show) if *uiid==1001 => {
@@ -37,7 +52,7 @@ fn start_client(addr: String, id:u32, name: String, tx_next: futures::sync::mpsc
                     }
                 },
                 S2C::RequestLoginInfo => {
-                    println!("request login");
+                    // println!("request login");
                     tx.send(C2S::ResponseLoginInfo(name.clone())).unwrap();
                     if let Err(e) = tx.flush() {
                         println!("disconnect. {} err {}", id, e);
@@ -45,9 +60,9 @@ fn start_client(addr: String, id:u32, name: String, tx_next: futures::sync::mpsc
                     }
                 },
                 S2C::ShowUI(uiid,show) if *uiid==2 => {
-                    println!("recv show ui 2");
-                    for i in 0..10 { //ここが大きすぎると返事がこない
-                        let mut text = (0..10).map(|_|"X").collect::<String>();
+                    // println!("recv show ui 2");
+                    for _ in 0..10 { //ここが大きすぎると返事がこない
+                        let text = (0..10).map(|_|"X").collect::<String>();
                         // text.insert_str(0, &text.clone());
                         // text.insert_str(0, &text.clone());
                         // text.insert_str(0, &text.clone());
@@ -63,13 +78,27 @@ fn start_client(addr: String, id:u32, name: String, tx_next: futures::sync::mpsc
                         println!("disconnect. {} err {}", id, e);
                         return Ok(false);
                     }
+                    send_at = Some(std::time::SystemTime::now());
                     // println!("send bye");
                     // return Ok(false);
                 },
                 S2C::AddText(uiid,text) => {
                     // println!("{:?} recv characters: {}", std::time::SystemTime::now(), text.len(), );
                     if text.ends_with(&bye_text) {
-                        println!("disconnect. {}", id);
+                        let mut t: u128 = 0;
+                        if let Some(at) = send_at {
+                            let time = at.elapsed().unwrap();
+                            t = time.as_millis();
+                            {
+                                let mut lock = CONNS_COUNT.lock().unwrap();
+                                *lock += 1;
+                            }
+                            {
+                                let mut lock = TIME_SUM.lock().unwrap();
+                                *lock += t;
+                            }
+                        }
+                        println!("disconnect. {} time {}", id, t);
                         return Ok(false);
                     }
                 }
@@ -90,7 +119,7 @@ fn start_client(addr: String, id:u32, name: String, tx_next: futures::sync::mpsc
         println!("connection error = {:?}", err);
     });
 
-    println!("start client {}", id);
+    // println!("start client {}", id);
     tokio::spawn(client);
 }
 
@@ -99,7 +128,7 @@ pub fn main() -> Result<(), Box<std::error::Error>> {
     let args:Vec<String> = env::args().collect();
     let addr = args[1].clone();
 
-    let (tx,rx) = futures::sync::mpsc::channel::<(u32,String)>(32);
+    let (tx,rx) = futures::sync::mpsc::channel::<(u32,String)>(1024);
     let tx2 = tx.clone();
 
     let mut ids = Vec::new();
@@ -108,17 +137,29 @@ pub fn main() -> Result<(), Box<std::error::Error>> {
     }
 
     let mut wait = tx.wait();
-    for i in 0..1 {
+    for i in 0..100 {
         wait.send((i, ids[i as usize].clone()));
     }
     // .map_err(|_|());
 
+    let report_time = Ok(()).into_future().and_then(|_|{
+        let task = tokio::timer::Interval::new(std::time::Instant::now(), std::time::Duration::from_secs(10))
+        .for_each(|_|{
+            let time = TIME_SUM.lock().unwrap();
+            let count = CONNS_COUNT.lock().unwrap();
+            println!("avg time: {}", *time as f64 / *count as f64);
+            Ok(())
+        })
+        .map_err(|_|());
+        tokio::spawn(task);
+        Ok(())
+    });
     {
         let start = rx.for_each(move|(id,name)| {
             start_client(addr.clone(), id, name, tx2.clone());
             Ok(())
         }); 
-        tokio::run(start);
+        tokio::run(report_time.and_then(|_| start));
     }
 
     Ok(())
